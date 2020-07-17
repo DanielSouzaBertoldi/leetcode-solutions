b3e99b5ddbebdfe2650237bab3d97f6ee0470f85
Daniel Bertoldi <dsdanielbertoldi@gmail.com>
2020-07-17 14:31:20 -0300

    Update reverseLinkedList.c

--- Recursion/reverseLinkedList.c
+++ Recursion/reverseLinkedList.c
@@ -13,6 +13,10 @@ Output: 5->4->3->2->1->NULL
 Runtime: 4 ms (beats 76.60% of C submissions)
 Memory Usage: 6 MB (beats 59.09% of C submissions)
 
+* Recursive
+Runtime: 4 ms (beats 76.60% of C submissions)
+Memory Usage: 6.1 MB (beats 19.79% of C submissions)
+
 *******************************************************************************/
 /**
  * Definition for singly-linked list.
@@ -21,7 +25,8 @@ Memory Usage: 6 MB (beats 59.09% of C submissions)
  *     struct ListNode *next;
  * };
  */
- 
+
+/* Iteravitvely */
 struct ListNode* reverseList(struct ListNode* head){
     // Iteratively
     struct ListNode *next = NULL, *previous = NULL, *current = head;
@@ -32,4 +37,16 @@ struct ListNode* reverseList(struct ListNode* head){
         current = next;
     }
     head = previous;
+}
+
+/* Recursive */
+struct ListNode* reverseList(struct ListNode* head){
+    // Recursive
+    if (head && head->next) {
+      struct ListNode* rest = reverseList(head->next);
+      head->next->next = head; 
+      head->next = NULL; 
+      return rest;
+    }  else
+      return head;
 }
